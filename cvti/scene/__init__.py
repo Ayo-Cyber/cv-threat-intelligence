c214e6e60a60b929f28cdcb8944ea520047d7367
Demilade Kolawole-Jacobs <jacobsdemilade@gmail.com>
2026-08-30 17:08:50 +0100

    feat: add site-scoped scene context store

--- cvti/scene/__init__.py
+++ cvti/scene/__init__.py
@@ -1,5 +1,6 @@
 """Scene context and environment mapping."""
 
 from cvti.scene.agent_mapper import AgentMapper
+from cvti.scene.context_store import SceneContextStore
 
-__all__ = ["AgentMapper"]
+__all__ = ["AgentMapper", "SceneContextStore"]
